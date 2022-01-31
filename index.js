8d6bd228ccc8dd7460933a9f1def28f08d87bbf0
Umadevi R <umad18323.cs@rmkec.ac.in>
2022-01-31 14:42:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { response } = require('express');
 const express=require('express');
 const fetch=require('node-fetch');
 const app=express();
-const port = 3000;
+const port = 80;
 
 app.use(express.static('public'));
 
